2b05bcf9bfb739eb7790009b5b93fc267008d929
David Berghammer <berghammer.david@gmail.com>
2019-10-28 09:01:45 +0100

    Fixed user ticket return format

--- middlewares/user/getUserTickets.js
+++ middlewares/user/getUserTickets.js
@@ -3,12 +3,27 @@ const ApplicationError = require('../../exceptions/ApplicationError');
 
 module.exports = function () {
     return function (req, res, next) {
-        models.User.findOne({where: {id: req.params.userId}, include: [models.BoughtTicket]}).then(function (user) {
+        models.User.findOne({where: {id: req.params.userId}, include: [{model: models.BoughtTicket, include: [{model: models.Ticket, include: [models.Line]}]}]}).then(function (user) {
             if(!user) {
                 return next(new ApplicationError(`User with id ${req.params.userId} does not exist`, 400));
             }
             return res.json(
-                user.BoughtTickets
+                user.BoughtTickets.map((ticket => ({
+                    id: ticket.id,
+                    validFrom: ticket.validFrom,
+                    validUntil: ticket.validUntil,
+                    ticketType: {
+                        typeId: ticket.Ticket.typeId,
+                        type: ticket.Ticket.type,
+                        validFor: ticket.Ticket.validFor,
+                        validTimeUnit: ticket.Ticket.validTimeUnit,
+                        line: {
+                            id: ticket.Ticket.Line.id,
+                            name: ticket.Ticket.Line.name,
+                            type: ticket.Ticket.Line.type
+                        }
+                    }
+                })))
             );
         });
     };
