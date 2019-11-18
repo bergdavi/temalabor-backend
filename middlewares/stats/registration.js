dc095c126066bb9f245750dbfff66101c94686af
David Berghammer <berghammer.david@gmail.com>
2019-11-18 09:16:34 +0100

    Updated stats endpoints to show correct data, Updated API docs

--- middlewares/stats/registration.js
+++ middlewares/stats/registration.js
@@ -4,9 +4,9 @@ const op = require('sequelize').Op;
 module.exports = function () {
     return function (req, res, next) {
         let until = new Date();
-        until.setHours(23,59,59,999);
+        until.setUTCHours(23,59,59,999);
         let from = new Date(until.getTime() - 1000 * 60 * 60 * 24 * 30);
-        from.setHours(0,0,0,0);
+        from.setUTCHours(0,0,0,0);
 
         models.User.findAll({where: {
                 [op.and]: [
@@ -20,16 +20,18 @@ module.exports = function () {
         }}).then(function (users) {
             let sales = [];
             let days = Math.floor((until - from)/(1000 * 60 * 60 * 24));
-            for(let i = 0; i < days; i++) {
-                let day = new Date(from.getTime() + 1000*60*60*24*(i+1));
+            for(let i = 0; i <= days; i++) {
+                let day = new Date(from.getTime() + 1000*60*60*24*(i));
                 sales.push({
-                    sold: 0,
+                    registered: 0,
                     day: `${day.getFullYear()}-${day.getMonth()+1}-${day.getDate()}`
                 })
             }
             users.forEach(function (user) {
-                let day = Math.floor((user.createdAt - from)/(1000 * 60 * 60 * 24));
-                sales[day].sold++;
+                let created = user.createdAt;
+                created.setUTCHours(0, 0, 0, 0);
+                let day = Math.round((created - from)/(1000 * 60 * 60 * 24));
+                sales[day].registered++;
             });
             res.json(sales);
         });
--- middlewares/stats/sales.js
+++ middlewares/stats/sales.js
@@ -5,9 +5,9 @@ const op = require('sequelize').Op;
 module.exports = function () {
     return function (req, res, next) {
         let until = new Date();
-        until.setHours(23,59,59,999);
+        until.setUTCHours(23,59,59,999);
         let from = new Date(until.getTime() - 1000 * 60 * 60 * 24 * 30);
-        from.setHours(0,0,0,0);
+        from.setUTCHours(0,0,0,0);
 
         models.BoughtTicket.findAll({where: {
                 [op.and]: [
@@ -21,15 +21,17 @@ module.exports = function () {
         }}).then(function (tickets) {
             let sales = [];
             let days = Math.floor((until - from)/(1000 * 60 * 60 * 24));
-            for(let i = 0; i < days; i++) {
-                let day = new Date(from.getTime() + 1000*60*60*24*(i+1));
+            for(let i = 0; i <= days; i++) {
+                let day = new Date(from.getTime() + 1000*60*60*24*(i));
                 sales.push({
                     sold: 0,
                     day: `${day.getFullYear()}-${day.getMonth()+1}-${day.getDate()}`
                 })
             }
             tickets.forEach(function (ticket) {
-                let day = Math.floor((ticket.createdAt - from)/(1000 * 60 * 60 * 24));
+                let created = ticket.createdAt;
+                created.setUTCHours(0, 0, 0, 0);
+                let day = Math.round((created - from)/(1000 * 60 * 60 * 24));
                 sales[day].sold++;
             });
             res.json(sales);
