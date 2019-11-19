3f8c32feed491ec9fa1886d8eb9d899e8501b90e
David Berghammer <berghammer.david@gmail.com>
2019-11-19 14:34:39 +0100

    Added income stats

---
+++ middlewares/stats/income.js
@@ -0,0 +1,42 @@
+const models  = require('../../models');
+const ApplicationError = require('../../exceptions/ApplicationError');
+const op = require('sequelize').Op;
+
+module.exports = function () {
+    return function (req, res, next) {
+        let until = new Date();
+        until.setUTCHours(23,59,59,999);
+        let from = new Date(until.getTime() - 1000 * 60 * 60 * 24 * 30);
+        from.setUTCHours(0,0,0,0);
+
+        models.BoughtTicket.findAll({where: {
+                [op.and]: [
+                    {
+                        createdAt: {[op.lte]: until}
+                    },
+                    {
+                        createdAt: {[op.gte]: from}
+                    }
+                ]
+        }, include: [models.Ticket]}).then(function (tickets) {
+            let sales = [];
+            let days = Math.floor((until - from)/(1000 * 60 * 60 * 24));
+            for(let i = 0; i <= days; i++) {
+                let day = new Date(from.getTime() + 1000*60*60*24*(i));
+                sales.push({
+                    income: 0,
+                    day: `${day.getFullYear()}-${day.getMonth()+1}-${day.getDate()}`
+                })
+            }
+            tickets.forEach(function (ticket) {
+                let created = ticket.createdAt;
+                created.setUTCHours(0, 0, 0, 0);
+                let day = Math.round((created - from)/(1000 * 60 * 60 * 24));
+                if(ticket.Ticket && ticket.Ticket.price) {
+                    sales[day].income += ticket.Ticket.price;
+                }
+            });
+            res.json(sales);
+        });
+    };
+};
--- routes/stats.js
+++ routes/stats.js
@@ -4,6 +4,7 @@ const checkUserLoginMW = require('../middlewares/authentication/checkUserLogin')
 const checkUserIdMW = require('../middlewares/authentication/checkUserId');
 const getSaleStats = require('../middlewares/stats/sales');
 const getRegistrationStats = require('../middlewares/stats/registration');
+const getIncomeStats = require('../middlewares/stats/income');
 
 router.get('/sales',
     checkUserLoginMW(),
@@ -15,6 +16,12 @@ router.get('/registrations',
     checkUserLoginMW(),
     checkUserIdMW('admin'),
     getRegistrationStats()
+);
+
+router.get('/income',
+    checkUserLoginMW(),
+    checkUserIdMW('admin'),
+    getIncomeStats()
 );
 
 module.exports = router;
