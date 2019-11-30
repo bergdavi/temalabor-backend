f607afcade600bdb0d2e485a80321cc9c71888b1
David Berghammer <berghammer.david@gmail.com>
2019-11-30 09:13:16 +0100

    Fixed hanging request in case of db issue

--- middlewares/user/checkUserNotExists.js
+++ middlewares/user/checkUserNotExists.js
@@ -4,12 +4,13 @@ const ApplicationError = require('../../exceptions/ApplicationError');
 module.exports = function () {
 
     return function (req, res, next) {
-        // TODO use unique constraint instead
         models.User.findOne({where: {email: req.body.email}}).then(function (user) {
             if(!user) {
                 return next();
             }
             return next(new ApplicationError(`User with email address '${req.body.email}' already exists`, 400));
+        }).catch(function (error) {
+            return next(new ApplicationError(`Database failure`, 500));
         });
     };
 };
